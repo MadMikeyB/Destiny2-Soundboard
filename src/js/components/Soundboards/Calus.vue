7294de2046816a19c1d7c6682247f43ba35da009
Michael Burton <me@mikeylicio.us>
2021-02-14 04:45:28 +0000

    Fix pathing?

--- src/js/components/Soundboards/Calus.vue
+++ src/js/components/Soundboards/Calus.vue
@@ -48,7 +48,7 @@
                 });
             },
             fetchTranscripts(name){
-                axios.get(`dist/media/${this.dirname}/transcripts.json`).then(({data}) => {
+                axios.get(`https://madmikeyb.github.io/Destiny2-Soundboard/dist/media/${this.dirname}/transcripts.json`).then(({data}) => {
                     // Get all Transcripts, then flatten to single array.
                     const response = [].concat(...data)
                     const arr = []
@@ -63,7 +63,7 @@
                 })
             },
             createAndPlayAudioElement(transcript) {
-                const audio = new Audio(`dist/media/${this.dirname}/${transcript.EntryHash}.mp3`);
+                const audio = new Audio(`https://raw.githubusercontent.com/MadMikeyB/Destiny2-Soundboard/master/dist/media/${this.dirname}/${transcript.EntryHash}.mp3`);
                 audio.play(); 
             },
             shorten(string, len) {
